1d660373dc9ae8075632309a2ba26f278bc1de2f
GatyzkayaGeka <gatyzkayageka@gmail.com>
2022-11-07 22:49:08 +0500

    step three second try

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
+import userName from '../src/cli.js';
 console.log("Welcome to the Brain Games!");
-import {userName} from '../src/cli.js';
-console.log('Hello, ${userName}!');
+const name = userName();
+console.log(`Hello, ${name}!`);
--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,6 @@
 import readlineSync from 'readline-sync';
 const userName = () => {
-  const name = readlineSync.question('May I have your name? ');
-};
-export default userName; // сделала именной,если не получится сделать по умолчанию
+    const name = readlineSync.question('May I have your name? ');
+    return name;
+  };
+export default userName;
